e267587797c7e6b5d3246a362baca9ab354dff5b
Elena Belyasnik <elena.belyasnik@yandex.ru>
2023-05-05 23:01:12 +0300

    new file:   Lesson_2/Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    boolean male = true;
+    String name = "John";
+    float height = 180.0f;
+    float weight = 80.0f;
+    int age = 30;
+
+    String walk() {
+        return "Идёт...";
+    }
+
+    String sit() {
+        return "Сидит...";
+    }
+
+    String run() {
+        return "Бежит...";
+    }
+
+    String talk() {
+        return "Говорит...";
+    }
+
+    String learnJava() {
+        return "Учит Java...";
+    }
+}
