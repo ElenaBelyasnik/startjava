feb7663b6509198c16d2e1bfc854281c042f3e1a
Elena Belyasnik <elena.belyasnik@yandex.ru>
2023-06-14 23:00:52 +0300

    Modify GuessNumberTest.java, GuessNumber.java, Player.java

--- src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/guess/GuessNumber.java
@@ -1,6 +1,5 @@
 package com.startjava.lesson_2_3_4.guess;
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,62 +8,60 @@ public class GuessNumber {
     private static final int MAX_VALUE = 100;
     private final Player player1;
     private final Player player2;
+    private boolean isWin;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
+        this.isWin = false;
     }
 
     public void play() {
         player1.clear();
         player2.clear();
-        Player currentPlayer = player1;
-        boolean isWin = false;
-        Random rnd = new Random();
-        int secretNumber = rnd.nextInt(100);
-        while (!isWin) {
-            if (currentPlayer != null) {
-                System.out.println("Числа, введённые игроком " + currentPlayer.getName() + " "
-                        + Arrays.toString(currentPlayer.getNumbers()));
-                int attempt = currentPlayer.getAttempt();
-                inputNumber(currentPlayer, attempt);
-                int currentNumber = currentPlayer.getNumbers()[attempt];
-                if (currentNumber > secretNumber) {
-                    System.out.println("Число " + currentNumber
-                            + " больше того, что загадал компьютер (" + secretNumber + ")");
-                    currentPlayer = changePlayer(currentPlayer);
-                } else if (currentNumber < secretNumber) {
-                    System.out.println("Число " + currentNumber
-                            + " меньше того, что загадал компьютер (" + secretNumber + ")");
-                    currentPlayer = changePlayer(currentPlayer);
-                } else {
-                    isWin = true;
-                    System.out.println("Игрок " + currentPlayer.getName() + " угадал число "
-                            + currentNumber + " c "
-                            + currentPlayer.getAttempt() + " попытки");
-                }
+        analyzeNumber();
+    }
 
+    private void analyzeNumber() {
+        Random rnd = new Random();
+        int secretNumber = rnd.nextInt(MAX_VALUE) + 1;
+        Player currentPlayer = player1;
+        while (isGameNotOver()) {
+            int currentNumber = inputNumber(currentPlayer);
+            if (currentNumber > secretNumber) {
+                System.out.println("Число " + currentNumber
+                        + " больше того, что загадал компьютер (" + secretNumber + ")");
+            } else if (currentNumber < secretNumber) {
+                System.out.println("Число " + currentNumber
+                        + " меньше того, что загадал компьютер (" + secretNumber + ")");
             } else {
-                break;
+                isWin = true;
+                System.out.println("Игрок " + currentPlayer.getName() + " угадал число "
+                        + currentNumber + " c "
+                        + currentPlayer.getAttempt() + " попытки");
+                printResults(player1);
+                printResults(player2);
             }
+            currentPlayer = changePlayer(currentPlayer);
         }
         if (!isWin) {
             System.out.println("Никто из игроков не угадал число.");
         }
-        printResults(player1);
-        printResults(player2);
     }
 
-    private void inputNumber(Player player, int attempt) {
+    private boolean isGameNotOver() {
+        return !isWin && (hasAttempt(player1) || hasAttempt(player2));
+    }
+
+    private int inputNumber(Player player) {
         System.out.println("Игрок " + player.getName() + " вводит число: ");
         int number = console.nextInt();
-        int index = player.getResultsLength();
-        player.addNumbers(number, index);
-        player.setAttempt(++attempt);
+        player.addNumber(number);
+        return number;
     }
 
-    public boolean isPlayerAttemptNotOver(Player player) {
-        boolean isNotOver = player.getAttempt() < player.getFullNumbers().length;
+    public boolean hasAttempt(Player player) {
+        boolean isNotOver = player.getAttempt() < Player.CAPACITY;
         if (!isNotOver) {
             System.out.println("У " + player.getName() + " закончились попытки");
         }
@@ -72,21 +69,17 @@ public class GuessNumber {
     }
 
     private Player changePlayer(Player player) {
-        if (player == player1 && isPlayerAttemptNotOver(player2)) {
+        if (player == player1) {
             return player2;
-        } else if (player == player2 && isPlayerAttemptNotOver(player1)) {
-            return player1;
-        } else if (isPlayerAttemptNotOver(player)) {
-            return player;
         }
-        return null;
+        return player1;
     }
 
     public void printResults(Player player) {
         System.out.print("Числа, названные игроком " + player.getName() + ": ");
-        int[] results = player.getNumbers();
-        for (int result : results) {
-            System.out.print(result + " ");
+        int[] numbers = player.getNumbers();
+        for (int number : numbers) {
+            System.out.print(number + " ");
         }
         System.out.println();
     }
--- src/com/startjava/lesson_2_3_4/guess/Player.java
+++ src/com/startjava/lesson_2_3_4/guess/Player.java
@@ -3,48 +3,32 @@ package com.startjava.lesson_2_3_4.guess;
 import java.util.Arrays;
 
 public class Player {
-    private static final int CAPACITY = 10;
+    public static final int CAPACITY = 10;
     private final String name;
-    private int[] numbers = new int[CAPACITY];
+    private final int[] numbers = new int[CAPACITY];
     private int attempt;
 
     public Player(String name) {
         this.name = name;
     }
 
-
-
     public String getName() {
         return name;
     }
 
     public int[] getNumbers() {
-        return Arrays.copyOf(numbers, getResultsLength());
+        return Arrays.copyOf(numbers, attempt);
     }
 
-    public void addNumbers(int number, int index) {
-        numbers[index] = number;
+    public void addNumber(int number) {
+        if (attempt < numbers.length) {
+            numbers[attempt] = number;
+        }
+        attempt++;
     }
 
     public int getAttempt() {
         return attempt;
-    }
-
-    public void setAttempt(int attempt) {
-        this.attempt = attempt;
-    }
-
-    public int getResultsLength() {
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] == 0) {
-                return i;
-            }
-        }
-        return 0;
-    }
-
-    public int[] getFullNumbers(){
-        return Arrays.copyOf(numbers, numbers.length);
     }
 
     public void clear() {
